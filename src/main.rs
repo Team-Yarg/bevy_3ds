543ded96b6ad995df3f7418a097f42b1c2cb2506
Natasha England-Elbro <natasha@natashaee.me>
2023-12-17 20:13:56 +0000

    chore: setup

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use bevy::{
+    app::{App, Startup},
+    core_pipeline::core_2d::Camera2dBundle,
+    ecs::system::Commands,
+    hierarchy::BuildChildren,
+    ui::{
+        node_bundles::{NodeBundle, TextBundle},
+        Style, Val,
+    },
+    DefaultPlugins,
+};
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+    app.add_systems(Startup, setup);
+    app.run();
+}
+
+fn setup(mut cmds: Commands) {
+    cmds.spawn(Camera2dBundle::default());
+    cmds.spawn(NodeBundle {
+        style: Style {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .with_children(|parent| {
+        parent.spawn(TextBundle::from_section("Hello, World", Default::default()));
+    });
+}
