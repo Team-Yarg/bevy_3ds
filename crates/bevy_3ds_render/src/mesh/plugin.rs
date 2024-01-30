e51b6de9d661aba179d41c7d7ab79366d62e34e6
Jhynjhiruu <cub3r12@gmail.com>
2024-01-30 14:37:31 +0000

    Fix incorrect rendering hierarchy

--- crates/bevy_3ds_render/src/mesh/plugin.rs
+++ crates/bevy_3ds_render/src/mesh/plugin.rs
@@ -1,14 +1,20 @@
 use bevy::{
     app::Plugin,
-    render::{mesh::Mesh, texture::Image},
+    render::{mesh::Mesh, texture::Image, RenderApp},
 };
 
-use crate::prep_asset::PrepareAssetsPlugin;
+use crate::{draw::AppDrawCommandsExtra, prep_asset::PrepareAssetsPlugin};
+
+use super::draw::MeshDraw;
 
 pub struct MeshPlugin;
 
 impl Plugin for MeshPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_plugins(PrepareAssetsPlugin::<Mesh, Image>::default());
+
+        if let Ok(render_app) = app.get_sub_app_mut(RenderApp) {
+            render_app.add_render_command::<MeshDraw>();
+        }
     }
 }
--- src/default_plugins.rs
+++ src/default_plugins.rs
@@ -39,12 +39,11 @@ impl PluginGroup for DefaultPlugins {
             });
         group = group.add(bevy::asset::AssetPlugin::default());
         {
-            group = group
-                .add(render::plugin::Render3dsPlugin)
-                .add(ImagePlugin::default());
+            group = group.add(ImagePlugin::default());
         }
         group = group.add(Bevy3dsPbrPlugin);
         group = group.add(render::plugin::CorePipeline3ds);
+        group = group.add(render::plugin::Render3dsPlugin);
         group = group.add(sprite::SpritesPlugin).add(TextPlugin);
         //group = group.add(UiPlugin::default());
         group
