8d04e50db84d8325b4b9957df31e715ce2e7d8c0
Jhynjhiruu <cub3r12@gmail.com>
2024-01-31 19:32:41 +0000

    Mess around with plugin order

--- src/default_plugins.rs
+++ src/default_plugins.rs
@@ -38,12 +38,13 @@ impl PluginGroup for DefaultPlugins {
                 ..Default::default()
             });
         group = group.add(bevy::asset::AssetPlugin::default());
-        {
-            group = group.add(ImagePlugin::default());
-        }
         group = group.add(Bevy3dsPbrPlugin);
+        {
+            group = group
+                .add(render::plugin::Render3dsPlugin)
+                .add(ImagePlugin::default());
+        }
         group = group.add(render::plugin::CorePipeline3ds);
-        group = group.add(render::plugin::Render3dsPlugin);
         group = group.add(sprite::SpritesPlugin).add(TextPlugin);
         //group = group.add(UiPlugin::default());
         group
