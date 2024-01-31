52c19597b64216f6461c33538338b138016be374
ferdinandhubbard981 <ferdinandhubbard@gmail.com>
2024-01-31 18:38:47 +0000

    added implementation of _3dsAxisSettings

--- crates/bevy_3ds_input/src/settings.rs
+++ crates/bevy_3ds_input/src/settings.rs
@@ -2,33 +2,7 @@ use bevy::reflect::{Reflect, std_traits::ReflectDefault};
 use bevy::ecs::system::Resource;
 use bevy::utils::HashMap;
 use crate::axis::_3dsAxis;
-/// Settings for a [`_3dsAxis`].
-///
-/// It is used inside of the [`_3dsInputSettings`] to define the sensitivity range and
-/// threshold for an axis.
-/// Values that are higher than `livezone_upperbound` will be rounded up to 1.0.
-/// Values that are lower than `livezone_lowerbound` will be rounded down to -1.0.
-/// Values that are in-between `deadzone_lowerbound` and `deadzone_upperbound` will be rounded
-/// to 0.0.
-/// Otherwise, values will not be rounded.
-///
-/// The valid range is `[-1.0, 1.0]`.
-#[derive(Debug, Clone, Reflect, PartialEq)]
-#[derive(Default)] // TODO: review this line: I don't know why this needs to be here, but this fixes a compile error
-#[reflect(Debug, Default)]
-pub struct _3dsAxisSettings {
-    /// Values that are higher than `livezone_upperbound` will be rounded up to 1.0.
-    livezone_upperbound: f32,
-    /// Positive values that are less than `deadzone_upperbound` will be rounded down to 0.0.
-    deadzone_upperbound: f32,
-    /// Negative values that are greater than `deadzone_lowerbound` will be rounded up to 0.0.
-    deadzone_lowerbound: f32,
-    /// Values that are lower than `livezone_lowerbound` will be rounded down to -1.0.
-    livezone_lowerbound: f32,
-    /// `threshold` defines the minimum difference between old and new values to apply the changes.
-    threshold: f32,
-}
-
+use thiserror::Error;
 
 /// Settings for all 3ds inputs.
 ///
@@ -73,4 +47,370 @@ impl _3dsInputSettings {
             .get(&axis)
             .unwrap_or(&self.default_axis_settings)
     }
+}
+
+
+
+/// Settings for a [`_3dsAxis`].
+///
+/// It is used inside of the [`_3dsSettings`] to define the sensitivity range and
+/// threshold for an axis.
+/// Values that are higher than `livezone_upperbound` will be rounded up to 1.0.
+/// Values that are lower than `livezone_lowerbound` will be rounded down to -1.0.
+/// Values that are in-between `deadzone_lowerbound` and `deadzone_upperbound` will be rounded
+/// to 0.0.
+/// Otherwise, values will not be rounded.
+///
+/// The valid range is `[-1.0, 1.0]`.
+#[derive(Debug, Clone, Reflect, PartialEq)]
+#[reflect(Debug, Default)]
+pub struct _3dsAxisSettings {
+    /// Values that are higher than `livezone_upperbound` will be rounded up to 1.0.
+    livezone_upperbound: f32,
+    /// Positive values that are less than `deadzone_upperbound` will be rounded down to 0.0.
+    deadzone_upperbound: f32,
+    /// Negative values that are greater than `deadzone_lowerbound` will be rounded up to 0.0.
+    deadzone_lowerbound: f32,
+    /// Values that are lower than `livezone_lowerbound` will be rounded down to -1.0.
+    livezone_lowerbound: f32,
+    /// `threshold` defines the minimum difference between old and new values to apply the changes.
+    threshold: f32,
+}
+
+impl Default for _3dsAxisSettings {
+    fn default() -> Self {
+        _3dsAxisSettings {
+            livezone_upperbound: 1.0,
+            deadzone_upperbound: 0.05,
+            deadzone_lowerbound: -0.05,
+            livezone_lowerbound: -1.0,
+            threshold: 0.01,
+        }
+    }
+}
+
+impl _3dsAxisSettings {
+    /// Creates a new [`_3dsAxisSettings`] instance.
+    ///
+    /// # Arguments
+    ///
+    /// + `livezone_lowerbound` - the value below which inputs will be rounded down to -1.0.
+    /// + `deadzone_lowerbound` - the value above which negative inputs will be rounded up to 0.0.
+    /// + `deadzone_upperbound` - the value below which positive inputs will be rounded down to 0.0.
+    /// + `livezone_upperbound` - the value above which inputs will be rounded up to 1.0.
+    /// + `threshold` - the minimum value by which input must change before the change is registered.
+    ///
+    /// Restrictions:
+    ///
+    /// + `-1.0 <= livezone_lowerbound <= deadzone_lowerbound <= 0.0`
+    /// + `0.0 <= deadzone_upperbound <= livezone_upperbound <= 1.0`
+    /// + `0.0 <= threshold <= 2.0`
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`_3dsAxisSettingsError`] if any restrictions on the zone values are not met.
+    /// If the zone restrictions are met, but the `threshold` value restrictions are not met,
+    /// returns [`_3dsAxisSettingsError::Threshold`].
+    pub fn new(
+        livezone_lowerbound: f32,
+        deadzone_lowerbound: f32,
+        deadzone_upperbound: f32,
+        livezone_upperbound: f32,
+        threshold: f32,
+    ) -> Result<_3dsAxisSettings, _3dsAxisSettingsError> {
+        if !(-1.0..=0.0).contains(&livezone_lowerbound) {
+            Err(_3dsAxisSettingsError::LiveZoneLowerBoundOutOfRange(
+                livezone_lowerbound,
+            ))
+        } else if !(-1.0..=0.0).contains(&deadzone_lowerbound) {
+            Err(_3dsAxisSettingsError::DeadZoneLowerBoundOutOfRange(
+                deadzone_lowerbound,
+            ))
+        } else if !(0.0..=1.0).contains(&deadzone_upperbound) {
+            Err(_3dsAxisSettingsError::DeadZoneUpperBoundOutOfRange(
+                deadzone_upperbound,
+            ))
+        } else if !(0.0..=1.0).contains(&livezone_upperbound) {
+            Err(_3dsAxisSettingsError::LiveZoneUpperBoundOutOfRange(
+                livezone_upperbound,
+            ))
+        } else if livezone_lowerbound > deadzone_lowerbound {
+            Err(
+                _3dsAxisSettingsError::LiveZoneLowerBoundGreaterThanDeadZoneLowerBound {
+                    livezone_lowerbound,
+                    deadzone_lowerbound,
+                },
+            )
+        } else if deadzone_upperbound > livezone_upperbound {
+            Err(
+                _3dsAxisSettingsError::DeadZoneUpperBoundGreaterThanLiveZoneUpperBound {
+                    livezone_upperbound,
+                    deadzone_upperbound,
+                },
+            )
+        } else if !(0.0..=2.0).contains(&threshold) {
+            Err(_3dsAxisSettingsError::Threshold(threshold))
+        } else {
+            Ok(Self {
+                livezone_lowerbound,
+                deadzone_lowerbound,
+                deadzone_upperbound,
+                livezone_upperbound,
+                threshold,
+            })
+        }
+    }
+
+    /// Get the value above which inputs will be rounded up to 1.0.
+    pub fn livezone_upperbound(&self) -> f32 {
+        self.livezone_upperbound
+    }
+
+    /// Try to set the value above which inputs will be rounded up to 1.0.
+    ///
+    /// # Errors
+    ///
+    /// If the value passed is less than the dead zone upper bound,
+    /// returns `_3dsAxisSettingsError::DeadZoneUpperBoundGreaterThanLiveZoneUpperBound`.
+    /// If the value passed is not in range [0.0..=1.0], returns `_3dsAxisSettingsError::LiveZoneUpperBoundOutOfRange`.
+    pub fn try_set_livezone_upperbound(&mut self, value: f32) -> Result<(), _3dsAxisSettingsError> {
+        if !(0.0..=1.0).contains(&value) {
+            Err(_3dsAxisSettingsError::LiveZoneUpperBoundOutOfRange(value))
+        } else if value < self.deadzone_upperbound {
+            Err(
+                _3dsAxisSettingsError::DeadZoneUpperBoundGreaterThanLiveZoneUpperBound {
+                    livezone_upperbound: value,
+                    deadzone_upperbound: self.deadzone_upperbound,
+                },
+            )
+        } else {
+            self.livezone_upperbound = value;
+            Ok(())
+        }
+    }
+
+    /// Try to set the value above which inputs will be rounded up to 1.0.
+    /// If the value passed is negative or less than `deadzone_upperbound`,
+    /// the value will not be changed.
+    ///
+    /// Returns the new value of `livezone_upperbound`.
+    pub fn set_livezone_upperbound(&mut self, value: f32) -> f32 {
+        self.try_set_livezone_upperbound(value).ok();
+        self.livezone_upperbound
+    }
+
+    /// Get the value below which positive inputs will be rounded down to 0.0.
+    pub fn deadzone_upperbound(&self) -> f32 {
+        self.deadzone_upperbound
+    }
+
+    /// Try to set the value below which positive inputs will be rounded down to 0.0.
+    ///
+    /// # Errors
+    ///
+    /// If the value passed is greater than the live zone upper bound,
+    /// returns `_3dsAxisSettingsError::DeadZoneUpperBoundGreaterThanLiveZoneUpperBound`.
+    /// If the value passed is not in range [0.0..=1.0], returns `_3dsAxisSettingsError::DeadZoneUpperBoundOutOfRange`.
+    pub fn try_set_deadzone_upperbound(&mut self, value: f32) -> Result<(), _3dsAxisSettingsError> {
+        if !(0.0..=1.0).contains(&value) {
+            Err(_3dsAxisSettingsError::DeadZoneUpperBoundOutOfRange(value))
+        } else if self.livezone_upperbound < value {
+            Err(
+                _3dsAxisSettingsError::DeadZoneUpperBoundGreaterThanLiveZoneUpperBound {
+                    livezone_upperbound: self.livezone_upperbound,
+                    deadzone_upperbound: value,
+                },
+            )
+        } else {
+            self.deadzone_upperbound = value;
+            Ok(())
+        }
+    }
+
+    /// Try to set the value below which positive inputs will be rounded down to 0.0.
+    /// If the value passed is negative or greater than `livezone_upperbound`,
+    /// the value will not be changed.
+    ///
+    /// Returns the new value of `deadzone_upperbound`.
+    pub fn set_deadzone_upperbound(&mut self, value: f32) -> f32 {
+        self.try_set_deadzone_upperbound(value).ok();
+        self.deadzone_upperbound
+    }
+
+    /// Get the value below which negative inputs will be rounded down to -1.0.
+    pub fn livezone_lowerbound(&self) -> f32 {
+        self.livezone_lowerbound
+    }
+
+    /// Try to set the value below which negative inputs will be rounded down to -1.0.
+    ///
+    /// # Errors
+    ///
+    /// If the value passed is less than the dead zone lower bound,
+    /// returns `_3dsAxisSettingsError::LiveZoneLowerBoundGreaterThanDeadZoneLowerBound`.
+    /// If the value passed is not in range [-1.0..=0.0], returns `_3dsAxisSettingsError::LiveZoneLowerBoundOutOfRange`.
+    pub fn try_set_livezone_lowerbound(&mut self, value: f32) -> Result<(), _3dsAxisSettingsError> {
+        if !(-1.0..=0.0).contains(&value) {
+            Err(_3dsAxisSettingsError::LiveZoneLowerBoundOutOfRange(value))
+        } else if value > self.deadzone_lowerbound {
+            Err(
+                _3dsAxisSettingsError::LiveZoneLowerBoundGreaterThanDeadZoneLowerBound {
+                    livezone_lowerbound: value,
+                    deadzone_lowerbound: self.deadzone_lowerbound,
+                },
+            )
+        } else {
+            self.livezone_lowerbound = value;
+            Ok(())
+        }
+    }
+
+    /// Try to set the value below which negative inputs will be rounded down to -1.0.
+    /// If the value passed is positive or greater than `deadzone_lowerbound`,
+    /// the value will not be changed.
+    ///
+    /// Returns the new value of `livezone_lowerbound`.
+    pub fn set_livezone_lowerbound(&mut self, value: f32) -> f32 {
+        self.try_set_livezone_lowerbound(value).ok();
+        self.livezone_lowerbound
+    }
+
+    /// Get the value above which inputs will be rounded up to 0.0.
+    pub fn deadzone_lowerbound(&self) -> f32 {
+        self.deadzone_lowerbound
+    }
+
+    /// Try to set the value above which inputs will be rounded up to 0.0.
+    ///
+    /// # Errors
+    ///
+    /// If the value passed is less than the live zone lower bound,
+    /// returns `_3dsAxisSettingsError::LiveZoneLowerBoundGreaterThanDeadZoneLowerBound`.
+    /// If the value passed is not in range [-1.0..=0.0], returns `_3dsAxisSettingsError::DeadZoneLowerBoundOutOfRange`.
+    pub fn try_set_deadzone_lowerbound(&mut self, value: f32) -> Result<(), _3dsAxisSettingsError> {
+        if !(-1.0..=0.0).contains(&value) {
+            Err(_3dsAxisSettingsError::DeadZoneLowerBoundOutOfRange(value))
+        } else if self.livezone_lowerbound > value {
+            Err(
+                _3dsAxisSettingsError::LiveZoneLowerBoundGreaterThanDeadZoneLowerBound {
+                    livezone_lowerbound: self.livezone_lowerbound,
+                    deadzone_lowerbound: value,
+                },
+            )
+        } else {
+            self.deadzone_lowerbound = value;
+            Ok(())
+        }
+    }
+
+    /// Try to set the value above which inputs will be rounded up to 0.0.
+    /// If the value passed is less than -1.0 or less than `livezone_lowerbound`,
+    /// the value will not be changed.
+    ///
+    /// Returns the new value of `deadzone_lowerbound`.
+    pub fn set_deadzone_lowerbound(&mut self, value: f32) -> f32 {
+        self.try_set_deadzone_lowerbound(value).ok();
+        self.deadzone_lowerbound
+    }
+
+    /// Get the minimum value by which input must change before the change is registered.
+    pub fn threshold(&self) -> f32 {
+        self.threshold
+    }
+
+    /// Try to set the minimum value by which input must change before the change is registered.
+    ///
+    /// # Errors
+    ///
+    /// If the value passed is not within [0.0..=2.0], returns `_3dsSettingsError::AxisThreshold`.
+    pub fn try_set_threshold(&mut self, value: f32) -> Result<(), _3dsAxisSettingsError> {
+        if !(0.0..=2.0).contains(&value) {
+            Err(_3dsAxisSettingsError::Threshold(value))
+        } else {
+            self.threshold = value;
+            Ok(())
+        }
+    }
+
+    /// Try to set the minimum value by which input must change before the changes will be applied.
+    /// If the value passed is not within [0.0..=2.0], the value will not be changed.
+    ///
+    /// Returns the new value of threshold.
+    pub fn set_threshold(&mut self, value: f32) -> f32 {
+        self.try_set_threshold(value).ok();
+        self.threshold
+    }
+
+    /// Clamps the `raw_value` according to the `_3dsAxisSettings`.
+    pub fn clamp(&self, new_value: f32) -> f32 {
+        if self.deadzone_lowerbound <= new_value && new_value <= self.deadzone_upperbound {
+            0.0
+        } else if new_value >= self.livezone_upperbound {
+            1.0
+        } else if new_value <= self.livezone_lowerbound {
+            -1.0
+        } else {
+            new_value
+        }
+    }
+
+    /// Determines whether the change from `old_value` to `new_value` should
+    /// be registered as a change, according to the [`_3dsAxisSettings`].
+    fn should_register_change(&self, new_value: f32, old_value: Option<f32>) -> bool {
+        if old_value.is_none() {
+            return true;
+        }
+
+        f32::abs(new_value - old_value.unwrap()) > self.threshold
+    }
+
+    /// Filters the `new_value` based on the `old_value`, according to the [`_3dsAxisSettings`].
+    ///
+    /// Returns the clamped `new_value` if the change exceeds the settings threshold,
+    /// and `None` otherwise.
+    pub fn filter(&self, new_value: f32, old_value: Option<f32>) -> Option<f32> {
+        let new_value = self.clamp(new_value);
+
+        if self.should_register_change(new_value, old_value) {
+            return Some(new_value);
+        }
+        None
+    }
+}
+
+
+/// Errors that occur when setting axis settings for 3ds input.
+#[derive(Error, Debug, PartialEq)]
+pub enum _3dsAxisSettingsError {
+    /// The given parameter `livezone_lowerbound` was not in range -1.0..=0.0.
+    #[error("invalid livezone_lowerbound {0}, expected value [-1.0..=0.0]")]
+    LiveZoneLowerBoundOutOfRange(f32),
+    /// The given parameter `deadzone_lowerbound` was not in range -1.0..=0.0.
+    #[error("invalid deadzone_lowerbound {0}, expected value [-1.0..=0.0]")]
+    DeadZoneLowerBoundOutOfRange(f32),
+    /// The given parameter `deadzone_lowerbound` was not in range -1.0..=0.0.
+    #[error("invalid deadzone_upperbound {0}, expected value [0.0..=1.0]")]
+    DeadZoneUpperBoundOutOfRange(f32),
+    /// The given parameter `deadzone_lowerbound` was not in range -1.0..=0.0.
+    #[error("invalid livezone_upperbound {0}, expected value [0.0..=1.0]")]
+    LiveZoneUpperBoundOutOfRange(f32),
+    /// Parameter `livezone_lowerbound` was not less than or equal to parameter `deadzone_lowerbound`.
+    #[error("invalid parameter values livezone_lowerbound {} deadzone_lowerbound {}, expected livezone_lowerbound <= deadzone_lowerbound", .livezone_lowerbound, .deadzone_lowerbound)]
+    LiveZoneLowerBoundGreaterThanDeadZoneLowerBound {
+        /// The value of the `livezone_lowerbound` parameter.
+        livezone_lowerbound: f32,
+        /// The value of the `deadzone_lowerbound` parameter.
+        deadzone_lowerbound: f32,
+    },
+    ///  Parameter `deadzone_upperbound` was not less than or equal to parameter `livezone_upperbound`.
+    #[error("invalid parameter values livezone_upperbound {} deadzone_upperbound {}, expected deadzone_upperbound <= livezone_upperbound", .livezone_upperbound, .deadzone_upperbound)]
+    DeadZoneUpperBoundGreaterThanLiveZoneUpperBound {
+        /// The value of the `livezone_upperbound` parameter.
+        livezone_upperbound: f32,
+        /// The value of the `deadzone_upperbound` parameter.
+        deadzone_upperbound: f32,
+    },
+    /// The given parameter was not in range 0.0..=2.0.
+    #[error("invalid threshold {0}, expected 0.0 <= threshold <= 2.0")]
+    Threshold(f32),
 }
