b6ee1269e151cae6ead1d11f4f82b3677c7281a2
Natasha England-Elbro <natasha@natashaee.me>
2024-01-31 23:39:04 +0000

    fix: not extracting meshes to render world

--- crates/bevy_3ds_render/src/mesh/plugin.rs
+++ crates/bevy_3ds_render/src/mesh/plugin.rs
@@ -1,7 +1,15 @@
+use std::collections::HashMap;
+
 use bevy::{
     app::Plugin,
-    render::{mesh::Mesh, texture::Image, RenderApp},
+    asset::Handle,
+    ecs::system::{Query, ResMut, Resource},
+    math::Mat4,
+    pbr::StandardMaterial,
+    render::{mesh::Mesh, texture::Image, Extract, ExtractSchedule, RenderApp},
+    transform::components::{GlobalTransform, Transform},
 };
+use log::debug;
 
 use crate::{draw::AppDrawCommandsExtra, prep_asset::PrepareAssetsPlugin};
 
@@ -14,7 +22,38 @@ impl Plugin for MeshPlugin {
         app.add_plugins(PrepareAssetsPlugin::<Mesh, Image>::default());
 
         if let Ok(render_app) = app.get_sub_app_mut(RenderApp) {
-            render_app.add_render_command::<MeshDraw>();
+            render_app
+                .add_render_command::<MeshDraw>()
+                .init_resource::<ExtractedMeshes>()
+                .add_systems(ExtractSchedule, extract_meshes);
         }
+    }
+}
+
+pub struct ExtractedMesh {
+    pub mesh: Handle<Mesh>,
+    pub transform: Mat4,
+    pub material: Handle<StandardMaterial>,
+}
+
+#[derive(Resource, Default)]
+pub struct ExtractedMeshes {
+    pub extracted: Vec<ExtractedMesh>,
+}
+
+fn extract_meshes(
+    mut extracted: ResMut<ExtractedMeshes>,
+    query: Extract<Query<(&Handle<Mesh>, &Handle<StandardMaterial>, &GlobalTransform)>>,
+) {
+    extracted.extracted.clear();
+
+    for (mesh_handle, material_handle, transform) in &query {
+        debug!("extract: {mesh_handle:?}");
+
+        extracted.extracted.push(ExtractedMesh {
+            mesh: mesh_handle.to_owned(),
+            transform: transform.compute_matrix(),
+            material: material_handle.to_owned(),
+        })
     }
 }
